4b8feeac6e18de986e3427de1ec507d81392f968
Ayoub060 <140269448+Ayoub060@users.noreply.github.com>
2026-05-04 07:56:12 +0200

    structuration du site v2.1

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let traductions = {};
 
+// Détecte automatiquement la page depuis le nom du fichier HTML
+const fichier = window.location.pathname.split('/').pop().replace('.html', '');
+const PAGE = fichier === '' ? 'home' : fichier;
+
 fetch('traductions.json')
   .then(res => res.json())
   .then(data => {
-    traductions = data;
+    traductions = data[PAGE];
   });
 
 function changerLangue(langue, el) {
   const t = traductions[langue];
   if (!t) return;
 
+  // Changer le title de l'onglet
+  if (t.title) document.title = t.title;
+
+  // Changer les textes visibles
   document.querySelectorAll('[data-traduction]').forEach(elem => {
     const cle = elem.getAttribute('data-traduction');
     if (t[cle]) elem.textContent = t[cle];
